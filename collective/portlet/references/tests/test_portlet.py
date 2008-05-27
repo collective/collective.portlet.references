df85b755ae48c0ed29ca9d86050ff6b319dfda67
Maurits van Rees <maurits@vanrees.org>
2008-05-27 13:13:18 +0000

    pyflakes/pep8

--- collective/portlet/references/tests/test_portlet.py
+++ collective/portlet/references/tests/test_portlet.py
@@ -12,14 +12,18 @@ from collective.portlet.references import referencesportlet
 
 from collective.portlet.references.tests.base import TestCase
 
+
 class TestPortlet(TestCase):
 
     def afterSetUp(self):
-        self.setRoles(('Manager',))
+        self.setRoles(('Manager', ))
 
     def test_portlet_type_registered(self):
-        portlet = getUtility(IPortletType, name='collective.portlet.references.ReferencesPortlet')
-        self.assertEquals(portlet.addview, 'collective.portlet.references.ReferencesPortlet')
+        portlet = getUtility(
+            IPortletType,
+            name='collective.portlet.references.ReferencesPortlet')
+        self.assertEquals(portlet.addview,
+                          'collective.portlet.references.ReferencesPortlet')
 
     def test_interfaces(self):
         # TODO: Pass any keyword arguments to the Assignment constructor
@@ -28,22 +32,27 @@ class TestPortlet(TestCase):
         self.failUnless(IPortletDataProvider.providedBy(portlet.data))
 
     def test_invoke_add_view(self):
-        portlet = getUtility(IPortletType, name='collective.portlet.references.ReferencesPortlet')
-        mapping = self.portal.restrictedTraverse('++contextportlets++plone.leftcolumn')
+        portlet = getUtility(
+            IPortletType,
+            name='collective.portlet.references.ReferencesPortlet')
+        mapping = self.portal.restrictedTraverse(
+            '++contextportlets++plone.leftcolumn')
         for m in mapping.keys():
             del mapping[m]
         addview = mapping.restrictedTraverse('+/' + portlet.addview)
 
-        # TODO: Pass a dictionary containing dummy form inputs from the add form
+        # TODO: Pass a dictionary containing dummy form inputs from the add
+        # form.
         # Note: if the portlet has a NullAddForm, simply call
         # addview() instead of the next line.
         addview.createAndAdd(data={})
 
         self.assertEquals(len(mapping), 1)
-        self.failUnless(isinstance(mapping.values()[0], referencesportlet.Assignment))
+        self.failUnless(isinstance(mapping.values()[0],
+                                   referencesportlet.Assignment))
 
-    # NOTE: This test can be removed if the portlet has no edit form
     def test_invoke_edit_view(self):
+        # NOTE: This test can be removed if the portlet has no edit form
         mapping = PortletAssignmentMapping()
         request = self.folder.REQUEST
 
@@ -55,36 +64,45 @@ class TestPortlet(TestCase):
         context = self.folder
         request = self.folder.REQUEST
         view = self.folder.restrictedTraverse('@@plone')
-        manager = getUtility(IPortletManager, name='plone.rightcolumn', context=self.portal)
+        manager = getUtility(IPortletManager, name='plone.rightcolumn',
+                             context=self.portal)
 
         # TODO: Pass any keyword arguments to the Assignment constructor
         assignment = referencesportlet.Assignment()
 
-        renderer = getMultiAdapter((context, request, view, manager, assignment), IPortletRenderer)
+        renderer = getMultiAdapter(
+            (context, request, view, manager, assignment), IPortletRenderer)
         self.failUnless(isinstance(renderer, referencesportlet.Renderer))
+
 
 class TestRenderer(TestCase):
 
     def afterSetUp(self):
-        self.setRoles(('Manager',))
+        self.setRoles(('Manager', ))
 
-    def renderer(self, context=None, request=None, view=None, manager=None, assignment=None):
+    def renderer(self, context=None, request=None, view=None, manager=None,
+                 assignment=None):
         context = context or self.folder
         request = request or self.folder.REQUEST
         view = view or self.folder.restrictedTraverse('@@plone')
-        manager = manager or getUtility(IPortletManager, name='plone.rightcolumn', context=self.portal)
+        manager = manager or getUtility(
+            IPortletManager, name='plone.rightcolumn', context=self.portal)
 
-        # TODO: Pass any default keyword arguments to the Assignment constructor
+        # TODO: Pass any default keyword arguments to the Assignment
+        # constructor.
         assignment = assignment or referencesportlet.Assignment()
-        return getMultiAdapter((context, request, view, manager, assignment), IPortletRenderer)
+        return getMultiAdapter((context, request, view, manager, assignment),
+                               IPortletRenderer)
 
     def test_render(self):
         # TODO: Pass any keyword arguments to the Assignment constructor
-        r = self.renderer(context=self.portal, assignment=referencesportlet.Assignment())
+        r = self.renderer(context=self.portal,
+                          assignment=referencesportlet.Assignment())
         r = r.__of__(self.folder)
         r.update()
         output = r.render()
         # TODO: Test output
+
 
 def test_suite():
     from unittest import TestSuite, makeSuite
